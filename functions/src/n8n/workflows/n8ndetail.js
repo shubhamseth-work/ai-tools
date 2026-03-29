4997b89da96cf945afdfc4b4ad2064920b74a71b
shubhangi <sd18@consultlane.com>
2026-03-29 10:33:14 +0530

    'OllamRestApi'

---
+++ functions/src/n8n/workflows/n8ndetail.js
@@ -0,0 +1,33 @@
+Inst App id - 3376083152546865
+Inst Secret - e5fcca4ebb3d2339895b91e97a6fcecc
+
+
+App Id - 1408725597474129
+App secret - 09d7d3de1f50b803ad3d485635edb5cc
+
+{
+  "id": "122096236443188120",
+  "name": "Prince Shukla"
+}
+
+
+{
+  "name": "Hills Natural",
+  "id": "890185440852501"
+}
+
+
+{
+  "instagram_business_account": {
+    "id": "17841479968425896"
+  },
+  "id": "890185440852501"
+}
+
+
+acces token - 
+
+EAAUBOnzSzVEBQfnvaao4BbcQkWdMSFfSiuo0m3oje7kAiSkeYx1yQnCZAbdSL6rUfid7xxJQ1lZB0BS16Y6Ds5bqZAbZBVZBKQAMKmNnYxMvJ1g4yh6KSmVxuTQ9DZCog9AZBZBxmY5KwtnY9A3KTY2XHgGMYf0wrtZAJ4XumIaAZCAKTnYeWqLVrX0dCt4veLW4WdddZB2WqMC57cXxwwzdQ1pCgSZCH7mDZCD11Riy7AimsYUN5DktghRZBTkZBesxVbKSvN9ZCqpuekHCGkL0Ht7ObgC9pRalBoAZD
+
+long lived token
+EAAUBOnzSzVEBQW4ZBaBwY77vPByPycH7fIZA7MxZAmPLBpsuMzfjfdsBTn6arZBxJAVjzXYXgesAZCOi54cveRtiZB5vox5xcfcqdF9kKHlh0LBBnSRBdgvYP8MM0bUiZAflYqIZBPlgVfLZAz7kLWfAmZAS79B7W59LZCXvfjawvuWIWCPQ3HMKsZBdTe4eFJ8qZCEnf
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ dotenv.config();
 import express from 'express';
 import axios from 'axios';
 import cors from 'cors';
-
+import OpenAI from "openai";
+import NewsAPI from 'newsapi';
  // Ensure you have newrelic installed and configured
 const app = express();
 import { getWeatherByCity } from './functions/src/weather/weatherService.js';
@@ -18,6 +19,8 @@ import { sendMsgAlerts } from './functions/src/gcp/pubsub/sendMsgAlerts.js';
 import { downloadPdfFromUrl } from './functions/src/scripts/downloadPdfByUrl.js';
 import { extractRecrodFromDocument } from './functions/src/scripts/extractDataFromPdf.js';
 import { unlockPdfDocument } from './functions/src/scripts/unlockPdfPassword.js';
+import ttsRouter from './functions/src/textToSpeech/convertTextToSpeech.js';
+import { sendOllamaPrompt } from './functions/src/ollama/ollamaRestApi.js';
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
@@ -164,6 +167,79 @@ app.get('/unlock-pdf',async (req,res)=>{
     return res.status(400).json({ error: error.message || 'Error in unlocking pdf'});
   }
 })
+
+app.get('/fetch-news',async (req,res)=>{
+  try {
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?apiKey=${process.env.NEWS_API_KEY}`, {
+      params: {
+        language: 'en',
+        country: 'us'
+      }
+    });
+    console.log('Fetched news data', response.data);
+    return res.status(200).json({ result: response.data });
+  } catch (error) {
+    console.error('Error in fetching news:', error);
+    return res.status(400).json({ error: error.message || 'Error in fetching news'});
+  }
+})
+
+app.get('/grok-openai',async (req,res)=>{
+  try {
+
+    const client = new OpenAI({
+        apiKey: process.env.GROQ_API_KEY,
+        baseURL: "https://api.groq.com/openai/v1",
+    });
+
+    const response = await client.responses.create({
+        model: "openai/gpt-oss-20b",
+        input: "Write a one line of poem of Shakespeare.",
+    });
+    return res.status(200).json({ result: response.output_text });
+  } catch (error) {
+    console.error('Error in fetching news:', error);
+    return res.status(400).json({ error: error.message || 'Error in fetching news'});
+  }
+})
+
+// Health check
+app.get("/ollama", (req, res) => {
+  res.send("Ollama API is running");
+});
+
+app.post('/ollama-chat',async (req,res)=>{
+  // Simple request limiter
+  let activeRequests = 0;
+  const MAX_REQUESTS = 2;
+  try {
+
+    const key = req.headers["x-api-key"];
+    if (key !== process.env.OLLAMA_API_KEY) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    if (activeRequests >= MAX_REQUESTS) {
+      return res.status(429).json({ error: "Server busy, try later" });
+    }
+    activeRequests++;
+
+    const input = req.body?.prompt || "What is the capital of France?";
+    console.log('Received Ollama prompt:',  input);
+
+    const output = await sendOllamaPrompt(input, req.body);
+    return res.status(200).json({ result: output });
+
+  } catch (error) {
+    console.error('Error in fetching news:', error);
+    return res.status(400).json({ error: error.message || 'Error in fetching news'});
+    
+  } finally {
+    activeRequests--;
+  }
+})
+app.use(ttsRouter);
+
 function listRoutes(app) {
     console.log("📌 Registered Routes:");
     app._router.stack
